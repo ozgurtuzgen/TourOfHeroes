a95421303b8e51bf9b1cfebec078ddc3160fbb03
ozgurtuzgen <ozgurtuzgen@hotmail.com>
2016-01-11 14:29:37 +0200

    EOF milestone3

--- app/crisis-center/mock-crisisList.ts
+++ app/crisis-center/mock-crisisList.ts
@@ -5,14 +5,8 @@
 import {Crisis} from './crisis';
 
 export var  CRISISLIST : Crisis[] = [
-    {"id":11,"name":"mr.Nice"},
-    {"id":12,"name":"Narco"},
-    {"id":13,"name":"Bombasto"},
-    {"id":14,"name":"Celeritas"},
-    {"id":15,"name":"Magneta"},
-    {"id":16,"name":"RubberMan"},
-    {"id":17,"name":"Dynama"},
-    {"id":18,"name":"Dr IQ"},
-    {"id":19,"name":"Magma"},
-    {"id":20,"name":"Tornado"}
+    {"id":1,"name":'Princess Held Captive'},
+    {"id":2,"name":'Dragon Burning Cities'},
+    {"id":3,"name":'Giant Asteroid Heading For Earth'},
+    {"id":4,"name":'Release Deadline Looms'}
 ];
