ab06b98969e137008702d697b18b2359a3147bca
ozgurtuzgen <ozgurtuzgen@hotmail.com>
2016-01-22 17:18:18 +0200

    jasmine 101

---
+++ app/tests/1st.spec.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by ozgur.tuzgen on 21.01.2016.
+ */
+describe("1st tests", ()=>{
+    it('true is true', () => expect(true).toEqual(true));
+
+    it('null is not the same thing with undefined',()=> expect(null).not.toEqual(undefined));
+})
